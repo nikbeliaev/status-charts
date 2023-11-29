3de4890268ad3c9ad3974d953869be5d5faec9bb
nbeliaev <nbeliaev.work@gmail.com>
2023-11-29 17:42:44 +0400

    fix/added upload link to nav

--- src/components/ChartsNav/ChartsNav.tsx
+++ src/components/ChartsNav/ChartsNav.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 
 import { NavSkeleton } from "@/components/Skeleton/Skeleton"
 import chartDataService from '@/services/chartsDataService'
+import { UserAuth } from '@/context/AuthContext';
 
 type ChartLinkProps = {
   href: string
@@ -21,6 +22,7 @@ function ChartLink({ href, text }: ChartLinkProps) {
 
 export default function ChartsNav() {
   const [links, setLinks] = useState<any>([])
+  const { user } = UserAuth();
 
   useEffect(() => {
     const fetchCharts = async () => {
@@ -40,7 +42,8 @@ export default function ChartsNav() {
     <nav className="w-full max-w-5xl bg-gray-50 px-6 py-10 min-h-screen dark:bg-gray-900 m-auto">
       <h1 className="font-bold text-2xl mb-4">Charts</h1>
       {links.length ? (
-        links.map((link: any[]) => {
+        <>
+        {links.map((link: any[]) => {
           return (
             <ChartLink
               href={`/charts/${link[0]}`}
@@ -48,7 +51,16 @@ export default function ChartsNav() {
               key={link[1].title}
             />
           )
-        })
+        })}
+        {user && (
+          <Link href="/upload" className="flex mt-20 items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
+            Import data from .csv
+            <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </Link>
+        )}
+        </>
       ) : (
         <NavSkeleton />
       )}
